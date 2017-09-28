793590741fcad0e0610a1ab31a5c3cb9c5753d27
Saketh Vallakatla <saketh21v@gmail.com>
2017-09-28 17:38:32 +0530

    Something

--- lib/Hero.py
+++ lib/Hero.py
@@ -1,46 +1,44 @@
 class Hero(object):
-    def __init__(self, board,xCoord, yCoord, drone):
+    def __init__(self, board, xCoord, yCoord, drone):
         self.xCoord = xCoord
         self.yCoord = yCoord
         self.board = board
         self.drone = drone
-    
+
     def moveSelf(self, direction):
-        if direction == 'left' :
-            if self.xCoord-1 < 0:
+        if direction == 'left':
+            if self.xCoord - 1 < 0:
                 return False
-            self.xCoord-=1
+            self.xCoord -= 1
         elif direction == 'right':
-            if self.xCoord+1 > self.board.xEdge:
+            if self.xCoord + 1 > self.board.xEdge:
                 return False
-            self.xCoord+=1
+            self.xCoord += 1
         elif direction == 'up':
-            if self.yCoord-1 < 0:
+            if self.yCoord - 1 < 0:
                 return False
-            self.yCoord-=1
+            self.yCoord -= 1
         elif direction == 'down':
-            if self.yCoord+1 > self.board.yEdge:
+            if self.yCoord + 1 > self.board.yEdge:
                 return False
-            self.yCoord+=1
+            self.yCoord += 1
         return True
 
     def moveDrone(self, direction):
         drone = self.drone
-        if direction == 'left' :
-            if drone.xCoord-1 < 0:
+        if direction == 'left':
+            if drone.xCoord - 1 < 0:
                 return "Invalid"
-            drone.xCoord-=1
+            drone.xCoord -= 1
         elif direction == 'right':
-            if drone.xCoord+1 > drone.board.xEdge:
+            if drone.xCoord + 1 > drone.board.xEdge:
                 return False
-            drone.xCoord+=1
+            drone.xCoord += 1
         elif direction == 'up':
-            if drone.yCoord-1 < 0:
+            if drone.yCoord - 1 < 0:
                 return False
-            drone.yCoord-=1
+            drone.yCoord -= 1
         elif direction == 'down':
-            if drone.yCoord+1 > drone.board.yEdge:
+            if drone.yCoord + 1 > drone.board.yEdge:
                 return False
-            drone.yCoord+=1
-        
-    
+            drone.yCoord += 1
